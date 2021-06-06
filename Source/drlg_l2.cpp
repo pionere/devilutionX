1a410f487ebaf82c3fb5d9d787dc77e4859e019a
pionere <pionere@freemail.hu>
2021-06-06 08:37:18 +0200

    bugfix for 'cleanup of the core functions of drlg_l2.cpp'

--- Source/drlg_l2.cpp
+++ Source/drlg_l2.cpp
@@ -1563,6 +1563,7 @@ const BYTE Patterns[][16] = {
 	// obsolete { 4, 1, 1, 1, 1, 1, 2, 1, 1, 14 },
 /*47*/	{ 5, 1, 1, 1, 1, 1, 2, 1, 1, 14 },
 /*48*/	{ 4, 1, 2, 1, 1, 3, 1, 1, 2, 8 }, // new
+/*49*/	{ 5, 1, 2, 1, 1, 1, 2, 1, 5, 15 }, // new - not an exact replacement. A mirror of 8 would be better, but does not exist yet.
 	// useless { 1, 1, 1, 1, 1, 1, 1, 1, 2, 8 },
 	//{ 0, 1, 0, 0, 3, 0, 0, 1, 0, 4 }, // vertical door
 	//{ 0, 0, 0, 1, 3, 1, 0, 0, 0, 5 }, // horizontal door
