0381727f4e447de3d93a458063e06e70fb85691b
pionere <pionere@freemail.hu>
2021-09-17 08:44:26 +0200

    bugfix for "integrate 'Fix inaccurate gameplay with NOSOUND'"

--- Source/effects.h
+++ Source/effects.h
@@ -6,6 +6,10 @@
 #ifndef __EFFECTS_H__
 #define __EFFECTS_H__
 
+#ifdef NOSOUND
+#include "engine.h"
+#endif
+
 DEVILUTION_BEGIN_NAMESPACE
 
 #ifdef __cplusplus
@@ -33,11 +37,11 @@ void effects_play_sound(const char *snd_file);
 #else
 inline bool effect_is_playing(int nSFX) { return false; }
 inline void stream_stop() { }
+inline void PlaySFX(int psfx, int rndCnt = 1)  { if (rndCnt != 1) random_(165, rndCnt); }
+inline void PlaySfxLoc(int psfx, int x, int y, int rndCnt = 1)  { PlaySFX(-1, rndCnt); }
 inline void InitMonsterSND(int midx) { }
 inline void FreeMonsterSnd()  { }
-inline void PlayEffect(int mnum, int mode)  { PlaySFX(2); }
-inline void PlaySFX(int psfx, int rndCnt = 1)  { if (rndCnt != 1) random_(165, rndCnt); }
-inline void PlaySfxLoc(int psfx, int x, int y, int rndCnt = 1)  { PlaySFX(rndCnt); }
+inline void PlayEffect(int mnum, int mode)  { PlaySFX(-1, 2); }
 inline void sound_stop() { }
 inline void sound_update() { }
 inline void effects_cleanup_sfx() { }
