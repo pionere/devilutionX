11a60f277e317685d451003a9c1a0031073af2f8
pionere <pionere@freemail.hu>
2021-11-06 16:20:14 +0100

    bugfix for 'improved shadows in catacombs'

--- Source/objects.cpp
+++ Source/objects.cpp
@@ -2700,7 +2700,7 @@ static void OperatePedistal(int pnum, int oi, bool sendmsg)
 		break;
 	case 4:
 		//ObjChangeMap(setpc_x, setpc_y, setpc_x + setpc_w, setpc_y + setpc_h);
-		ObjChangeMap(setpc_x + 2, setpc_y, setpc_x + 6, setpc_y + 8);
+		ObjChangeMap(setpc_x /*+ 2*/, setpc_y, setpc_x + 9/*6*/, setpc_y + 8);
 		LoadMapSetObjs("Levels\\L2Data\\Blood2.DUN");
 		os->_oSelFlag = 0;
 		break;
@@ -4021,7 +4021,7 @@ static void SyncPedistal(int oi)
 		break;
 	case 4:
 		//ObjChangeMapResync(setpc_x, setpc_y, setpc_x + setpc_w, setpc_y + setpc_h);
-		ObjChangeMapResync(setpc_x + 2, setpc_y, setpc_x + 6, setpc_y + 8);
+		ObjChangeMapResync(setpc_x, setpc_y, setpc_x + 9, setpc_y + 8);
 		LoadMapSetObjs("Levels\\L2Data\\Blood2.DUN");
 		break;
 	default:
