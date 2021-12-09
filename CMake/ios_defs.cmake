3bb920247b576acb2267671e31e90929e4b4eb6b
pionere <pionere@freemail.hu>
2021-12-09 12:39:39 +0100

    integrate '[ios] Set up build process' + 'Parallel build of iOS port' + '[ios] Fix ipa bundle format' 8192434beadcc9fda7e1553fedad1b7d491c1161 e7728cb2162ad06a0e6f7480b5123c0ef14624c6 a821a1eb0c96fc2e67456457c958cb4ab3aa37a4

---
+++ CMake/ios_defs.cmake
@@ -0,0 +1,18 @@
+# General build options.
+#set(VIRTUAL_GAMEPAD ON)
+set(ZEROTIER OFF)
+set(NOSOUND ON)
+
+# Disable all system dependencies.
+# All of these will be fetched via FetchContent and linked statically.
+set(DEVILUTIONX_SYSTEM_SDL2 OFF)
+#set(DEVILUTIONX_STATIC_SDL2 ON)
+#set(DEVILUTIONX_SYSTEM_SDL_IMAGE OFF)
+#set(DEVILUTIONX_SYSTEM_SDL_AUDIOLIB OFF)
+set(DEVILUTIONX_SYSTEM_LIBSODIUM OFF)
+#set(DEVILUTIONX_SYSTEM_LIBPNG OFF)
+#set(DEVILUTIONX_SYSTEM_LIBFMT OFF)
+
+# Disable sanitizers. They're not supported out-of-the-box.
+set(ASAN OFF)
+set(UBSAN OFF)
