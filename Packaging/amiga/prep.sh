cc099eff2602d2bafd91e9f8c7a02f421b59a5f0
pionere <pionere@freemail.hu>
2021-12-14 12:27:33 +0100

    integrate 'Amiga: Cleanup build process' 5986a081c368994427f24ee14d457ccede2bf7f9

--- Packaging/amiga/prep.sh
+++ Packaging/amiga/prep.sh
@@ -13,6 +13,17 @@ export M68K_COMMON="-s -ffast-math -fomit-frame-pointer"
 export M68K_CFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
 export M68K_CXXFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
 
+PARALLELISM="$(getconf _NPROCESSORS_ONLN)"
+
+declare -ra CMAKE_FLAGS=(
+  -DM68K_CPU="$M68K_CPU"
+  -DM68K_FPU="$M68K_FPU"
+  -DM68K_COMMON="$M68K_COMMON"
+  -DBUILD_SHARED_LIBS=OFF
+  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_INSTALL_PREFIX="${SYSROOT}/usr"
+)
+
 mkdir -p deps
 mkdir -p ${SYSROOT}/usr/lib
 mkdir -p ${SYSROOT}/usr/include
@@ -22,16 +33,16 @@ cd deps
 wget https://www.zlib.net/zlib-1.2.11.tar.gz -O zlib-1.2.11.tar.gz
 tar -xvf zlib-1.2.11.tar.gz
 mkdir -p zlib-1.2.11/build
-cd zlib-1.2.11/build
-cmake .. -DM68K_CPU="$M68K_CPU" -DM68K_FPU="$M68K_FPU" -DM68K_COMMON="${M68K_COMMON} -O3 -fno-exceptions -w -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
-cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
-cd ../..
+cd zlib-1.2.11
+cmake -S. -Bbuild "${CMAKE_FLAGS[@]}" -O3 -fno-exceptions -w -noixemul -DBIG_ENDIAN -DAMIGA -fpermissive -std=c++14"
+cmake --build build -j"$PARALLELISM" --config Release --target install
+cd ..
 
 # SDL1.2
 wget https://github.com/AmigaPorts/libSDL12/archive/master.tar.gz -O SDL-1.2.tar.gz
 tar -xvf SDL-1.2.tar.gz
 cd libSDL12-master
-make PREFX=${SYSROOT} PREF=${SYSROOT} -j$(getconf _NPROCESSORS_ONLN)
+make PREFX=${SYSROOT} PREF=${SYSROOT} -j"$PARALLELISM"
 mkdir -p ${SYSROOT}/usr/lib
 mkdir -p ${SYSROOT}/usr/include
 cp -fvr libSDL.a ${SYSROOT}/usr/lib/
@@ -49,13 +60,13 @@ cd ..
 #cd ..
 
 # FreeType
-wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz -O freetype-2.10.1.tar.gz
-tar -xvf freetype-2.10.1.tar.gz
-mkdir -p freetype-2.10.1/build
-cd freetype-2.10.1/build
-cmake .. -DUNIX=1 -DM68K_CPU="$M68K_CPU" -DM68K_FPU="$M68K_FPU" -DM68K_COMMON="${M68K_COMMON}"
-cmake --build . --config Release --target install -- -j$(getconf _NPROCESSORS_ONLN)
-cd ../..
+#wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz -O freetype-2.10.1.tar.gz
+#tar -xvf freetype-2.10.1.tar.gz
+#mkdir -p freetype-2.10.1/build
+#cd freetype-2.10.1
+#cmake -S. -Bbuild -DUNIX=1 -DM68K_CPU="$M68K_CPU" -DM68K_FPU="$M68K_FPU" -DM68K_COMMON="${M68K_COMMON}"
+#cmake --build build -j"$PARALLELISM" --config Release --target install
+#cd ..
 
 # SDL_ttf
 #wget https://github.com/SDL-mirror/SDL_ttf/archive/SDL-1.2.tar.gz -O SDL_ttf-SDL-1.2.tar.gz
