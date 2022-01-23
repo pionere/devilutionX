c6a59ba96cf18d500f75030bf6ba991261ee6ef7
pionere <pionere@freemail.hu>
2022-01-23 15:15:35 +0100

    simplify handling of SDL_TEXTINPUT

--- Source/miniwin/misc_msg.cpp
+++ Source/miniwin/misc_msg.cpp
@@ -1025,10 +1025,14 @@ bool PeekMessage(LPMSG lpMsg)
 	case SDL_TEXTEDITING:
 		return FalseAvail("SDL_TEXTEDITING", e.edit.length);
 #endif // DEBUG_MODE
-	case SDL_TEXTINPUT:
+	case SDL_TEXTINPUT: {
+		uint32_t inputChar;
+		int err;
+		//lpMsg->wParam = utf8_to_latin1(e.text.text).c_str()[0];
+		utf8_decode((unsigned char*)&e.text.text[0], &inputChar, &err);
+		lpMsg->wParam = err ? '?' : inputChar;
 		lpMsg->message = DVL_WM_CHAR;
-		lpMsg->wParam = utf8_to_latin1(e.text.text).c_str()[0];
-		break;
+	} break;
 	case SDL_WINDOWEVENT:
 		switch (e.window.event) {
 		case SDL_WINDOWEVENT_SHOWN:
