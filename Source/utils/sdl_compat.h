95db57b6ed951e6e15279a6dddaae7b9a20134b8
pionere <pionere@freemail.hu>
2022-01-31 11:18:48 +0100

    add back 'Optimize masked Art blitting' ce66aea07058de5bd855af2efb3828d84fe378a6 + always use #ifdef USE_SDL1 in sdl_compat.h

--- Source/utils/sdl_compat.h
+++ Source/utils/sdl_compat.h
@@ -31,18 +31,19 @@
 
 inline const Uint8 *SDLC_GetKeyState()
 {
-#ifndef USE_SDL1
-	return SDL_GetKeyboardState(NULL);
-#else
+#ifdef USE_SDL1
 	return SDL_GetKeyState(NULL);
+#else
+	return SDL_GetKeyboardState(NULL);
 #endif
 }
 
 inline int SDLC_SetColorKey(SDL_Surface *surface, Uint32 key)
 {
 #ifdef USE_SDL1
-	return SDL_SetColorKey(surface, SDL_SRCCOLORKEY, key);
+	return SDL_SetColorKey(surface, SDL_SRCCOLORKEY | SDL_RLEACCEL, key);
 #else
+	SDL_SetSurfaceRLE(surface, 1);
 	return SDL_SetColorKey(surface, SDL_TRUE, key);
 #endif
 }
