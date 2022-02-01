0dd2a132b9f020ddc378b3008f18fb7c3ce737dd
pionere <pionere@freemail.hu>
2022-02-01 15:15:54 +0100

    support SSE2 and AVX to convert AUDIO_U8 to AUDIO_S16LSB

--- 3rdParty/SDL2_mixer/src/utils.c
+++ 3rdParty/SDL2_mixer/src/utils.c
@@ -385,7 +385,6 @@ void Mix_Converter_AUDIO16_Mono2Stereo_SSE2(Mix_BuffOps* buf)
     }
 }
 #endif // _SSE2_
-
 void Mix_Converter_AUDIO16_Mono2Stereo(Mix_BuffOps* buf)
 {
     Sint16* srcPos = (Sint16*)buf->endPos;
@@ -435,7 +434,6 @@ void Mix_Converter_AUDIO8_Resample_Half_SSE2(Mix_BuffOps* buf)
     }
 }
 #endif
-
 void Mix_Converter_AUDIO8_Resample_Half(Mix_BuffOps* buf)
 {
     Uint8* srcPos = (Uint8*)buf->currPos;
@@ -490,7 +488,6 @@ void Mix_Converter_AUDIO16_Resample_Half_SSE2(Mix_BuffOps* buf)
     }
 }
 #endif
-
 void Mix_Converter_AUDIO16_Resample_Half(Mix_BuffOps* buf)
 {
     Sint16* srcPos = (Sint16*)buf->currPos;
@@ -508,6 +505,84 @@ void Mix_Converter_AUDIO16_Resample_Half(Mix_BuffOps* buf)
     }
 }
 
+#ifdef __AVX__
+void Mix_Converter_U8_S16LSB_AVX(Mix_BuffOps* buf)
+{
+    Uint8* srcPos = (Uint8*)buf->endPos;
+    Uint8* currPos = (Uint8*)buf->currPos;
+
+    Uint8* dstPos = srcPos + (srcPos - currPos);
+    buf->endPos = dstPos;
+
+    const __m256i zero = _mm256_setzero_si256();
+    const __m256i sub = _mm256_set1_epi16(128);
+
+    while (&currPos[32] <= srcPos) {
+        srcPos -= 32;
+        dstPos -= 32;
+
+        __m256i aa = _mm256_loadu_si256((__m256i*)srcPos);
+
+        __m256i bb = _mm256_unpackhi_epi8(aa, zero);// zfill(a[hi], 16)
+        bb = _mm256_sub_epi16(bb, sub);				// subtract 128
+
+        bb = _mm256_slli_epi16(bb, 8);				// shl 8
+        _mm256_storeu_si256((__m256i*)dstPos, bb);
+
+        dstPos -= 32;
+        __m256i cc = _mm256_unpacklo_epi8(aa, zero);// zfill(a[lo], 16)
+        cc = _mm256_sub_epi16(cc, sub);				// subtract 128
+
+        cc = _mm256_slli_epi16(cc, 8);				// shl 8
+        _mm256_storeu_si256((__m256i*)dstPos, cc);
+    }
+
+    while (srcPos != currPos) {
+        srcPos--;
+        dstPos -= 2;
+        *(Sint16*)dstPos = SDL_SwapLE16((*srcPos - 128) << 8);
+    }
+}
+#endif // __AVX__
+#ifdef __SSE2__
+void Mix_Converter_U8_S16LSB_SSE2(Mix_BuffOps* buf)
+{
+    Uint8* srcPos = (Uint8*)buf->endPos;
+    Uint8* currPos = (Uint8*)buf->currPos;
+
+    Uint8* dstPos = srcPos + (srcPos - currPos);
+    buf->endPos = dstPos;
+
+    const __m128i zero = _mm_setzero_si128();
+    const __m128i sub = _mm_set1_epi16(128);
+
+    while (&currPos[16] <= srcPos) {
+        srcPos -= 16;
+        dstPos -= 16;
+
+        __m128i aa = _mm_loadu_si128((__m128i*)srcPos);
+
+        __m128i bb = _mm_unpackhi_epi8(aa, zero);	// zfill(a[hi], 16)
+        bb = _mm_sub_epi16(bb, sub);				// subtract 128
+
+        bb = _mm_slli_epi16(bb, 8);					// shl 8
+        _mm_storeu_si128((__m128i*)dstPos, bb);
+
+        dstPos -= 16;
+        __m128i cc = _mm_unpacklo_epi8(aa, zero);	// zfill(a[lo], 16)
+        cc = _mm_sub_epi16(cc, sub);				// subtract 128
+
+        cc = _mm_slli_epi16(cc, 8);					// shl 8
+        _mm_storeu_si128((__m128i*)dstPos, cc);
+    }
+
+    while (srcPos != currPos) {
+        srcPos--;
+        dstPos -= 2;
+        *(Sint16*)dstPos = SDL_SwapLE16((*srcPos - 128) << 8);
+    }
+}
+#endif
 void Mix_Converter_U8_S16LSB(Mix_BuffOps* buf)
 {
     Uint8* srcPos = (Uint8*)buf->endPos;
@@ -519,7 +594,7 @@ void Mix_Converter_U8_S16LSB(Mix_BuffOps* buf)
     while (srcPos != currPos) {
         srcPos--;
         dstPos -= 2;
-        *(Sint16*)dstPos = SDL_SwapLE16((*srcPos - 128) * 32768 / 128);
+        *(Sint16*)dstPos = SDL_SwapLE16((*srcPos - 128) << 8);
     }
 }
 
@@ -532,12 +607,14 @@ void Mix_Utils_Init()
         Mix_Convert_AUDIO8_Resample_Half = Mix_Converter_AUDIO8_Resample_Half_SSE2;
         Mix_Convert_AUDIO16_Resample_Half = Mix_Converter_AUDIO16_Resample_Half_SSE2;
         Mix_MixAudioFormat = Mix_Mixer_AUDIOS16_SSE2;
+        Mix_Convert_U8_S16LSB = Mix_Converter_U8_S16LSB_SSE2;
     }
 #endif
 #if defined(__AVX__) && SDL_VERSION_ATLEAST(2, 0, 2)
     if (SDL_HasAVX()) {
         Mix_Convert_AUDIO16_Mono2Stereo = Mix_Converter_AUDIO16_Mono2Stereo_AVX;
         Mix_MixAudioFormat = Mix_Mixer_AUDIOS16_AVX;
+        Mix_Convert_U8_S16LSB = Mix_Converter_U8_S16LSB_AVX;
     }
 #endif
 }
