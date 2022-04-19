4d8776f9d8f563839b4046532e741a4f31cee320
pionere <pionere@freemail.hu>
2022-04-19 10:32:03 +0200

    bugfix for devilutionx (FindTrigger vs DLV_HELL1) - eliminate special handling of the warp to hell in town

--- Source/controls/plrctrls.cpp
+++ Source/controls/plrctrls.cpp
@@ -388,8 +388,6 @@ static void FindTrigger()
 		for (int i = 0; i < numtrigs; i++) {
 			int tx = trigs[i]._tx;
 			int ty = trigs[i]._ty;
-			if (trigs[i]._tlvl == DLV_HELL1)
-				ty -= 1;
 			const int newDdistance = GetDistance(tx, ty, 2);
 			if (newDdistance < 0)
 				continue;
