d169bca1049b911365c753cd6cb2dfc39432a12a
pionere <pionere@freemail.hu>
2022-05-25 10:59:52 +0200

    fix limits of window positions - SCREEN_X/Y must be ignored - prevent windows from 'overflowing' the end of the back-buffer

--- Source/gameui.cpp
+++ Source/gameui.cpp
@@ -14,6 +14,9 @@ int gnWndBookX, gnWndBookY;
 int gnWndTeamX, gnWndTeamY;
 int gnWndQuestX, gnWndQuestY;
 
+#define WND_CHECK_X(x, dx, w)	((x + dx) >= SCREEN_WIDTH - w + BORDER_LEFT || (x + dx) < -BORDER_LEFT)
+#define WND_CHECK_Y(y, dy, h)	((y + dy) >= SCREEN_HEIGHT - (h + 1) + BORDER_BOTTOM || (y + dy) < -BORDER_TOP)
+
 void InitGameUI()
 {
 	gnWndBeltX = BELT_LEFT, gnWndBeltY = BELT_TOP;
@@ -36,52 +39,52 @@ void InitGameUI()
 	getIniInt("GameUI", "QuestX", &gnWndQuestX);
 	getIniInt("GameUI", "QuestY", &gnWndQuestY);
 
-	if (gnWndBeltX >= SCREEN_X + SCREEN_WIDTH - BELT_WIDTH + BORDER_LEFT || gnWndBeltX < -BORDER_LEFT) {
-		DoLog("BeltX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBeltX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - BELT_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndBeltX, 0, BELT_WIDTH)) {
+		DoLog("BeltX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBeltX, -BORDER_LEFT, SCREEN_WIDTH - BELT_WIDTH + BORDER_LEFT);
 		gnWndBeltX = 0;
 	}
-	if (gnWndBeltY >= SCREEN_Y + SCREEN_HEIGHT - BELT_HEIGHT + BORDER_BOTTOM || gnWndBeltY < -BORDER_TOP) {
-		DoLog("BeltY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBeltY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - BELT_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndBeltY, 0, BELT_HEIGHT)) {
+		DoLog("BeltY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBeltY, -BORDER_TOP, SCREEN_HEIGHT - (BELT_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndBeltY = 0;
 	}
-	if (gnWndInvX >= SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT || gnWndInvX < -BORDER_LEFT) {
-		DoLog("InvX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndInvX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndInvX, 0, SPANEL_WIDTH)) {
+		DoLog("InvX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndInvX, -BORDER_LEFT, SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
 		gnWndInvX = 0;
 	}
-	if (gnWndInvY >= SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM || gnWndInvY < -BORDER_TOP) {
-		DoLog("InvY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndInvY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndInvY, 0, SPANEL_HEIGHT)) {
+		DoLog("InvY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndInvY, -BORDER_TOP, SCREEN_HEIGHT - (SPANEL_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndInvY = 0;
 	}
-	if (gnWndCharX >= SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT || gnWndCharX < -BORDER_LEFT) {
-		DoLog("CharX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndCharX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndCharX, 0, SPANEL_WIDTH)) {
+		DoLog("CharX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndCharX, -BORDER_LEFT, SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
 		gnWndCharX = 0;
 	}
-	if (gnWndCharY >= SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM || gnWndCharY < -BORDER_TOP) {
-		DoLog("CharY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndCharY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndCharY, 0, SPANEL_HEIGHT)) {
+		DoLog("CharY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndCharY, -BORDER_TOP, SCREEN_HEIGHT - (SPANEL_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndCharY = 0;
 	}
-	if (gnWndBookX >= SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT || gnWndBookX < -BORDER_LEFT) {
-		DoLog("BookX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBookX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndBookX, 0, SPANEL_WIDTH)) {
+		DoLog("BookX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBookX, -BORDER_LEFT, SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
 		gnWndBookX = 0;
 	}
-	if (gnWndBookY >= SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM || gnWndBookY < -BORDER_TOP) {
-		DoLog("BookY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBookY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndBookY, 0, SPANEL_HEIGHT)) {
+		DoLog("BookY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndBookY, -BORDER_TOP, SCREEN_HEIGHT - (SPANEL_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndBookY = 0;
 	}
-	if (gnWndTeamX >= SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT || gnWndTeamX < -BORDER_LEFT) {
-		DoLog("TeamX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndTeamX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndTeamX, 0, SPANEL_WIDTH)) {
+		DoLog("TeamX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndTeamX, -BORDER_LEFT, SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
 		gnWndTeamX = 0;
 	}
-	if (gnWndTeamY >= SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM || gnWndTeamY < -BORDER_TOP) {
-		DoLog("TeamY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndTeamY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndTeamY, 0, SPANEL_HEIGHT)) {
+		DoLog("TeamY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndTeamY, -BORDER_TOP, SCREEN_HEIGHT - (SPANEL_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndTeamY = 0;
 	}
-	if (gnWndQuestX >= SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT || gnWndQuestX < -BORDER_LEFT) {
-		DoLog("QuesX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndQuestX, -BORDER_LEFT, SCREEN_X + SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
+	if (WND_CHECK_X(gnWndQuestX, 0, SPANEL_WIDTH)) {
+		DoLog("QuesX(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndQuestX, -BORDER_LEFT, SCREEN_WIDTH - SPANEL_WIDTH + BORDER_LEFT);
 		gnWndQuestX = 0;
 	}
-	if (gnWndQuestY >= SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM || gnWndQuestY < -BORDER_TOP) {
-		DoLog("QuesY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndQuestY, -BORDER_TOP, SCREEN_Y + SCREEN_HEIGHT - SPANEL_HEIGHT + BORDER_BOTTOM);
+	if (WND_CHECK_Y(gnWndQuestY, 0, SPANEL_HEIGHT)) {
+		DoLog("QuesY(GameUI) setting(%d) is ignored because it is out of range (%d .. %d).", gnWndQuestY, -BORDER_TOP, SCREEN_HEIGHT - (SPANEL_HEIGHT + 1) + BORDER_BOTTOM);
 		gnWndQuestY = 0;
 	}
 }
