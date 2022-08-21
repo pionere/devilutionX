42360522e684563bc8343b5a01d059ef898c2726
pionere <pionere@freemail.hu>
2022-08-21 09:08:10 +0200

    reorganize spell-book - add Charge, Attract 'spells' - move fire-ring spell to the front of the 'hellfire' page - swap golem and stone spells - move 'unique' spells/skills (hbolt, flare, flash, telekinesis, stone) to the last page

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -80,6 +80,7 @@ void GetDamageAmt(int sn, int sl, int *minv, int *maxv)
 	case SPL_RATTACK:
 	case SPL_POINT_BLANK:
 	case SPL_FAR_SHOT:
+	case SPL_CHARGE:
 	case SPL_RAGE:
 	case SPL_STONE:
 	case SPL_INFRA:
