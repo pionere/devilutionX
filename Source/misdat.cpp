f991d93c6e12b3bd198e3d94bf2123690caf28e1
pionere <pionere@freemail.hu>
2022-10-03 11:36:41 +0200

    add MIF_ARROW flag to replace miSubType II.

--- Source/misdat.cpp
+++ Source/misdat.cpp
@@ -12,9 +12,9 @@ DEVILUTION_BEGIN_NAMESPACE
 const MissileData missiledata[] = {
 	// clang-format off
 // mName               mAddProc,                   mProc,              mDraw, mType,                          mdFlags, mResist,        mFileNum,        mlSFX,       miSFX,       mlSFXCnt, miSFXCnt
-/*MIS_ARROW*/        { &AddArrow,                  &MI_Arrow,          TRUE,      0,                         MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
-/*MIS_PBARROW*/      { &AddArrow,                  &MI_Arrow,          TRUE,      0,                         MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
-/*MIS_ASARROW*/      { &AddArrow,                  &MI_AsArrow,        TRUE,      0,                         MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
+/*MIS_ARROW*/        { &AddArrow,                  &MI_Arrow,          TRUE,      0,             MIF_ARROW | MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
+/*MIS_PBARROW*/      { &AddArrow,                  &MI_Arrow,          TRUE,      0,             MIF_ARROW | MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
+/*MIS_ASARROW*/      { &AddArrow,                  &MI_AsArrow,        TRUE,      0,             MIF_ARROW | MIF_LEAD, MISR_NONE,      MFILE_ARROWS,    SFX_NONE,    SFX_NONE,    1,        1, ALIGN },
 /*MIS_ARROWC*/       { &AddArrowC,                 &MI_Dummy,          FALSE,     0,                                0, MISR_NONE,      MFILE_NONE,      PS_BFIRE,    SFX_NONE,    1,        1, ALIGN },
 /*MIS_PBARROWC*/     { &AddArrowC,                 &MI_Dummy,          FALSE,     0,                                0, MISR_NONE,      MFILE_NONE,      PS_BFIRE,    SFX_NONE,    1,        1, ALIGN },
 /*MIS_ASARROWC*/     { &AddArrowC,                 &MI_Dummy,          FALSE,     0,                                0, MISR_NONE,      MFILE_NONE,      PS_BFIRE,    SFX_NONE,    1,        1, ALIGN },
