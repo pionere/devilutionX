dff556db7d805544d87ee789d61f445a7ee33877
pionere <pionere@freemail.hu>
2022-11-29 09:50:10 +0100

    no need for NULL-check after new

--- Source/dvlnet/frame_queue.cpp
+++ Source/dvlnet/frame_queue.cpp
@@ -78,8 +78,8 @@ buffer_t* frame_queue::make_frame(buffer_t packetbuf)
 	//if (size > (MAX_FRAME_SIZE - sizeof(uint32_t)))
 	//	app_error(ERR_APP_FRAME_BUFSIZE);
 	ret = new buffer_t(sizeof(uint32_t) + size);
-	if (ret == NULL)
-		app_error(ERR_APP_FRAME_BUFSIZE);
+	//if (ret == NULL)
+	//	app_error(ERR_APP_FRAME_BUFSIZE);
 	BYTE* data = ret->data();
 	*(uint32_t*)data = SwapLE32(size);
 	memcpy(data + sizeof(uint32_t), packetbuf.data(), size);
