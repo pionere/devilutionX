a404876d7f601b81b2b56118933d3adda49f04bc
pionere <pionere@freemail.hu>
2023-05-02 12:04:28 +0200

    test

--- Packaging/nix/debian-host-prep.sh
+++ Packaging/nix/debian-host-prep.sh
@@ -4,5 +4,5 @@ set -x
 
 sudo apt-get update
 sudo apt-get install -y \
-  rpm pkg-config cmake g++ git smpq gettext libsdl2-dev libsdl2-image-dev libsodium-dev \
-  libpng-dev libbz2-dev libfmt-dev
+  rpm pkg-config cmake g++ git smpq gettext libsdl2-dev libsodium-dev \
+  libpng-dev libbz2-dev
