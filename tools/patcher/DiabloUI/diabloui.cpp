9031141b7a12f6c6d3ab90b23e7c434813f9fa8b
pionere <pionere@freemail.hu>
2023-10-19 12:44:29 +0200

    call SpeakText only after the list is updated (patcher)

--- tools/patcher/DiabloUI/diabloui.cpp
+++ tools/patcher/DiabloUI/diabloui.cpp
@@ -69,13 +69,13 @@ void UiInitScreen(unsigned listSize, void (*fnFocus)(unsigned index), void (*fnS
 	gfnListSelect = fnSelect;
 	gfnListEsc = fnEsc;
 	gfnListDelete = NULL;
+	if (fnFocus != NULL)
+		fnFocus(SelectedItem);
 #if SCREEN_READER_INTEGRATION
 	if (gUIListItems.size() > SelectedItem) {
 		SpeakText(gUIListItems[SelectedItem]->m_text);
 	}
 #endif
-	if (fnFocus != NULL)
-		fnFocus(SelectedItem);
 
 #if FULL_UI
 	gUiEditField = NULL;
@@ -142,15 +142,15 @@ void UiFocus(unsigned itemIndex)
 	SelectedItem = itemIndex;
 
 	UiScrollIntoView();
-#if SCREEN_READER_INTEGRATION
-	if (gUIListItems.size() > SelectedItem) {
-		SpeakText(gUIListItems[SelectedItem]->m_text);
-	}
-#endif
 	UiPlayMoveSound();
 
 	if (gfnListFocus != NULL)
 		gfnListFocus(itemIndex);
+#if SCREEN_READER_INTEGRATION
+	if (gUIListItems.size() > itemIndex) {
+		SpeakText(gUIListItems[itemIndex]->m_text);
+	}
+#endif
 }
 
 static void UiFocusUp()
