936c43770c22092e5fad0f8c55d8201d272f0651
pionere <pionere@freemail.hu>
2023-11-30 17:03:18 +0100

    use enum instead of atomic::bool to maintain the status of the zt-network

--- Source/dvlnet/zerotier_native.cpp
+++ Source/dvlnet/zerotier_native.cpp
@@ -1,7 +1,6 @@
 #include "zerotier_native.h"
 #ifdef ZEROTIER
 #include <SDL.h>
-#include <atomic>
 
 #ifdef USE_SDL1
 #include "utils/sdl2_to_1_2_backports.h"
@@ -23,7 +22,12 @@ namespace net {
 //static constexpr uint64_t zt_earth = 0x8056c2e21c000001;
 static constexpr uint64_t ZtNetwork = 0xaf78bf943649eb12;
 
-static std::atomic_bool zt_network_ready(false);
+typedef enum zerotier_status : uint8_t {
+	ZT_READY,
+	ZT_CONNECT,
+	ZT_DOWN,
+} zerotier_status;
+static zerotier_status zt_status = ZT_DOWN;
 
 static void zerotier_event_handler(void* ptr)
 {
@@ -31,18 +35,17 @@ static void zerotier_event_handler(void* ptr)
 	switch (msg->event_code) {
 	case ZTS_EVENT_NODE_ONLINE:
 		// DoLog("ZeroTier: ZTS_EVENT_NODE_ONLINE");
-		// zt_node_online = true; -- don't change, zt-nodes are coming and going...
+		// zt_status = ZT_READY; -- don't change, zt-nodes are coming and going...
 		break;
 	case ZTS_EVENT_NODE_OFFLINE:
 		// DoLog("ZeroTier: ZTS_EVENT_NODE_OFFLINE");
-		// zt_node_online = false; -- don't change, they are just joking...
+		// zt_status = ZT_DOWN; -- don't change, they are just joking...
 		break;
 	case ZTS_EVENT_NETWORK_READY_IP6:
-		DoLog("ZeroTier: ZTS_EVENT_NETWORK_READY_IP6, networkId=%llx", (unsigned long long)msg->network->net_id);
+		// DoLog("ZeroTier: ZTS_EVENT_NETWORK_READY_IP6, networkId=%llx", (unsigned long long)msg->network->net_id);
+		// print_ip6_addr(&msg->network->assigned_addrs[0]);
 		multicast_join();
-		zt_network_ready = true;
-	case ZTS_EVENT_ADDR_ADDED_IP6:
-		print_ip6_addr(&(msg->addr->addr));
+		zt_status = ZT_READY;
 		break;
 	case ZTS_EVENT_NODE_UP:
 		// DoLog("ZeroTier: ZTS_EVENT_NODE_UP");
@@ -53,16 +56,20 @@ static void zerotier_event_handler(void* ptr)
 
 bool zerotier_network_ready()
 {
-	return zt_network_ready;
+	// return zts_addr_is_assigned(ZtNetwork, ZTS_AF_INET);
+	return zt_status == ZT_READY;
 }
 
 void zerotier_network_start()
 {
-	std::string ztpath = GetPrefPath();
-	ztpath += "zerotier";
-	zts_init_from_storage(ztpath.c_str());
-	zts_init_set_event_handler(&zerotier_event_handler);
-	zts_node_start();
+	if (zt_status == ZT_DOWN) {
+		zt_status = ZT_CONNECT;
+		std::string ztpath = GetPrefPath();
+		ztpath += "zerotier";
+		zts_init_from_storage(ztpath.c_str());
+		zts_init_set_event_handler(&zerotier_event_handler);
+		zts_node_start();
+	}
 }
 
 void zerotier_network_stop()
