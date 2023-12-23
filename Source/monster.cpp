334fb65de8df21a8c1627f474fa1071fb0ffaeb3
pionere <pionere@freemail.hu>
2023-12-23 10:55:43 +0100

    bugfix for 'cleanup of MonsterStructs and monster initialization' (less intrusive handling of gargoyles)

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -3670,7 +3670,7 @@ void MAI_Garg(int mnum)
 				return;
 			}
 		}
-		if (mon->_mmode != MM_SPATTACK) {
+		if (mon->_mmode != MM_SPATTACK && mon->_mmode != MM_STONE) {
 			if (mon->_mleaderflag == MLEADER_NONE) {
 				MonStartSpAttack(mnum);
 				mon->_mFlags |= MFLAG_LOCK_ANIMATION;
