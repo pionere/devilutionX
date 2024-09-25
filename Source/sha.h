d5952e7ea9bcfc5b762c2a75c8c0ed6500d5bbb5
pionere <pionere@freemail.hu>
2024-09-25 10:19:24 +0200

    use a single sha1-context

--- Source/sha.h
+++ Source/sha.h
@@ -12,14 +12,14 @@ DEVILUTION_BEGIN_NAMESPACE
 extern "C" {
 #endif
 
-#define SHA1ContextNum 3
+#define SHA1ContextNum 1
 #define SHA1HashSize   20
 #define SHA1BlockSize  64
 
 void SHA1Clear();
-void SHA1Result(int n, BYTE Message_Digest[SHA1HashSize]);
-void SHA1Calculate(int n, const BYTE* data, BYTE Message_Digest[SHA1HashSize]);
-void SHA1Reset(int n);
+void SHA1Result(/*int n, */BYTE Message_Digest[SHA1HashSize]);
+void SHA1Calculate(/*int n, */const BYTE* data, BYTE Message_Digest[SHA1HashSize]);
+void SHA1Reset(/*int n*/);
 
 #ifdef __cplusplus
 }
