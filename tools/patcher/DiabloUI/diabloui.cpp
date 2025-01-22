5afc22ef926ec0c68fe92f83fd75f23d84ce1d16
pionere <pionere@freemail.hu>
2025-01-22 13:08:13 +0100

    adjust spoken text with ListOffset (patcher) e28f539bf77350d9ae722c09063598d136cbc332

--- tools/patcher/DiabloUI/diabloui.cpp
+++ tools/patcher/DiabloUI/diabloui.cpp
@@ -99,13 +99,13 @@ void UiInitScreen(unsigned listSize, void (*fnFocus)(unsigned index), void (*fnS
 	gfnListSelect = fnSelect;
 	gfnListEsc = fnEsc;
 	gfnListDelete = NULL;
-	if (fnFocus != NULL)
+	if (fnFocus != NULL) {
 		fnFocus(SelectedItem);
 #if SCREEN_READER_INTEGRATION
-	if (gUIListItems.size() > SelectedItem) {
-		SpeakText(gUIListItems[SelectedItem]->m_text);
-	}
+		unsigned idx = SelectedItem - ListOffset;
+		SpeakText(gUIListItems[idx]->m_text);
 #endif
+	}
 
 #if FULL_UI
 	gUiEditField = NULL;
@@ -174,13 +174,13 @@ void UiFocus(unsigned itemIndex)
 	UiScrollIntoView();
 	UiPlayMoveSound();
 
-	if (gfnListFocus != NULL)
+	if (gfnListFocus != NULL) {
 		gfnListFocus(itemIndex);
 #if SCREEN_READER_INTEGRATION
-	if (gUIListItems.size() > itemIndex) {
-		SpeakText(gUIListItems[itemIndex]->m_text);
-	}
+		unsigned idx = itemIndex - ListOffset;
+		SpeakText(gUIListItems[idx]->m_text);
 #endif
+	}
 }
 
 static void UiFocusUp()
