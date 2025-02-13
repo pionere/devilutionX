c4c95cd788efeba16041e411dc0a681076c921d5
pionere <pionere@freemail.hu>
2025-02-13 15:33:13 +0100

    make globals static in towners.cpp

--- Source/towners.cpp
+++ Source/towners.cpp
@@ -7,10 +7,10 @@
 
 DEVILUTION_BEGIN_NAMESPACE
 
-int _guCowMsg;
+static int _guCowMsg;
 int numtowners;
-unsigned _guCowClicks;
-BYTE* pCowCels;
+static unsigned _guCowClicks;
+static BYTE* pCowCels;
 
 /**
  * Maps from active cow sound effect index and player class to sound
@@ -19,7 +19,7 @@ BYTE* pCowCels;
  * ref: enum _sfx_id
  * ref: enum plr_class
  */
-const int snSFX[3][NUM_CLASSES] = {
+static const int snSFX[3][NUM_CLASSES] = {
 	// clang-format off
 #ifdef HELLFIRE
 	{ PS_WARR52, PS_ROGUE52, PS_MAGE52, PS_MONK52, PS_ROGUE52, PS_WARR52 },
@@ -34,7 +34,7 @@ const int snSFX[3][NUM_CLASSES] = {
 };
 
 /** Specifies the animation frame sequence of a given NPC. */
-const int8_t AnimOrder[6][144] = {
+static const int8_t AnimOrder[6][144] = {
 	// clang-format off
 	{ 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
 	    14, 13, 12, 11, 10, 9, 8, 7, 6, 5,
