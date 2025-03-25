529e43b40976aefb86514cec0255cb1c6fd9af00
pionere <pionere@freemail.hu>
2025-03-25 07:52:18 +0100

    reduce package-dependencies of the debian-builds

--- Packaging/nix/debian-cross-aarch64-prep.sh
+++ Packaging/nix/debian-cross-aarch64-prep.sh
@@ -20,9 +20,9 @@ fi
 
 
 PACKAGES=(
-  cmake git smpq gettext
+  cmake git
   libsdl2-dev:arm64 libsodium-dev:arm64
-  libpng-dev:arm64 libbz2-dev:arm64 libspeechd-dev:arm64
+  libbz2-dev:arm64 libspeechd-dev:arm64
 )
 
 if (( $# < 1 )) || [[ "$1" != --no-gcc ]]; then
