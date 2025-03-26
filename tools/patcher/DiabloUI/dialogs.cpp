d824e3d233af966e16cf578aacd514b00acf5ba0
pionere <pionere@freemail.hu>
2025-03-26 19:32:13 +0100

    do not use PANEL_TOP/PANEL_LEFT in dialogs.cpp

--- tools/patcher/DiabloUI/dialogs.cpp
+++ tools/patcher/DiabloUI/dialogs.cpp
@@ -169,13 +169,13 @@ static void Init(const char* caption, char* text, bool error/*, const std::vecto
 		gbDialogBackCel = CelLoadImage(error ? "ui_art\\srpopup.CEL" : "ui_art\\spopup.CEL", SMALL_POPUP_WIDTH);
 		WordWrapArtStr(text, 240, AFT_SMALL);
 
-		SDL_Rect rect1 = { PANEL_MIDX(SMALL_POPUP_WIDTH), (PANEL_TOP + 168), SMALL_POPUP_WIDTH, SMALL_POPUP_HEIGHT };
+		SDL_Rect rect1 = { PANEL_MIDX(SMALL_POPUP_WIDTH), PANEL_MIDY(SMALL_POPUP_HEIGHT), SMALL_POPUP_WIDTH, SMALL_POPUP_HEIGHT };
 		gUiItems.push_back(new UiImage(gbDialogBackCel, 0, rect1, false));
 
-		SDL_Rect rect2 = { PANEL_LEFT + 200, (PANEL_TOP + 211), 240, 80 };
+		SDL_Rect rect2 = { PANEL_MIDX(SMALL_POPUP_WIDTH - 40), PANEL_MIDY(SMALL_POPUP_HEIGHT) + 43, SMALL_POPUP_WIDTH - 40, SMALL_POPUP_HEIGHT - SML_BUTTON_HEIGHT - 19 - 17 };
 		gUiItems.push_back(new UiText(text, rect2, UIS_LEFT | UIS_SMALL | UIS_GOLD));
 
-		SDL_Rect rect3 = { PANEL_MIDX(SML_BUTTON_WIDTH), (PANEL_TOP + 265), SML_BUTTON_WIDTH, SML_BUTTON_HEIGHT };
+		SDL_Rect rect3 = { PANEL_MIDX(SML_BUTTON_WIDTH), (PANEL_MIDY(SMALL_POPUP_HEIGHT) + SMALL_POPUP_HEIGHT - SML_BUTTON_HEIGHT - 19), SML_BUTTON_WIDTH, SML_BUTTON_HEIGHT };
 		gUiItems.push_back(new UiButton("OK", &DialogActionOK, rect3));
 	} else {*/
 		gbDialogBackCel = CelLoadImage(error ? "ui_art\\lrpopup.CEL" : "ui_art\\lpopup.CEL", LARGE_POPUP_WIDTH);
@@ -184,7 +184,7 @@ static void Init(const char* caption, char* text, bool error/*, const std::vecto
 		SDL_Rect rect1 = { PANEL_MIDX(LARGE_POPUP_WIDTH), PANEL_MIDY(LARGE_POPUP_HEIGHT), LARGE_POPUP_WIDTH, LARGE_POPUP_HEIGHT };
 		gUiItems.push_back(new UiImage(gbDialogBackCel, 0, rect1, false));
 
-		SDL_Rect rect2 = { PANEL_LEFT + 0, PANEL_MIDY(LARGE_POPUP_HEIGHT) + 10, PANEL_WIDTH, 20 };
+		SDL_Rect rect2 = { 0, PANEL_MIDY(LARGE_POPUP_HEIGHT) + 10, SCREEN_WIDTH, 20 };
 		gUiItems.push_back(new UiText(caption, rect2, UIS_HCENTER | UIS_MED | UIS_GOLD));
 
 		SDL_Rect rect3 = { PANEL_MIDX(LARGE_POPUP_TEXT_WIDTH), PANEL_MIDY(LARGE_POPUP_HEIGHT) + 41, LARGE_POPUP_TEXT_WIDTH, LARGE_POPUP_HEIGHT - SML_BUTTON_HEIGHT - 17 - 17 };
