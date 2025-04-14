26d9a27dfba03161f5c8bf03573c3a9f2d7aacad
pionere <pionere@freemail.hu>
2025-04-14 18:05:56 +0200

    add separate defines for the textbox of the minitext

--- Source/minitext.cpp
+++ Source/minitext.cpp
@@ -23,6 +23,10 @@ static int scrolltexty;
 #define MINITEXT_LINE_HEIGHT  38
 #define MINITEXT_PNL_X_OFFSET 24
 #define MINITEXT_PNL_Y_OFFSET 21
+#define MINITEXT_PANEL_WIDTH  591
+#define MINITEXT_PANEL_HEIGHT 303
+#define MINITEXT_PANEL_X      PANEL_CENTERX(MINITEXT_PANEL_WIDTH)
+#define MINITEXT_PANEL_Y      (PANEL_CENTERY(MINITEXT_PANEL_HEIGHT) - 64)
 
 static_assert(1900 / MINITEXT_LINE_HEIGHT == 50, "minitxtdata must be timed.");
 
@@ -39,7 +43,7 @@ void StartQTextMsg(int m, bool showText)
 		gbQtextflag = true;
 		gbActionBtnDown = 0;
 		qtextptr = tds->txtstr;
-		qtexty = (TPANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET) + MINITEXT_LINE_HEIGHT - 7;
+		qtexty = (MINITEXT_PANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET) + MINITEXT_LINE_HEIGHT - 7;
 		scrolltexty = tds->txtdelay;
 		qtextTime = SDL_GetTicks() + scrolltexty;
 	}
@@ -64,17 +68,17 @@ void DrawQText()
 	Uint32 currTime;
 	BYTE *pStart, *pEnd;
 
-	sx = LTPANEL_X;
-	sy = LTPANEL_Y;
+	sx = MINITEXT_PANEL_X;
+	sy = MINITEXT_PANEL_Y;
 
-	DrawColorTextBox(sx, sy, LTPANEL_WIDTH, TPANEL_HEIGHT, COL_GOLD);
+	DrawColorTextBox(sx, sy, MINITEXT_PANEL_WIDTH, MINITEXT_PANEL_HEIGHT, COL_GOLD);
 
 	/// ASSERT: assert(gpBuffer != NULL);
 	// TODO: create a function in engine to draw with a given height? (similar to DrawFlask2 in control.cpp)
 	pStart = gpBufStart;
 	gpBufStart = &gpBuffer[BUFFER_WIDTH * (sy + MINITEXT_PNL_Y_OFFSET)];
 	pEnd = gpBufEnd;
-	gpBufEnd = &gpBuffer[BUFFER_WIDTH * (sy + TPANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET)];
+	gpBufEnd = &gpBuffer[BUFFER_WIDTH * (sy + MINITEXT_PANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET)];
 
 	str = qtextptr;
 	pnl = NULL;
@@ -97,7 +101,7 @@ void DrawQText()
 			len += bigFontWidth[c] + FONT_KERN_BIG;
 			if (c == 0) // allow wordwrap on blank glyph
 				endstr = sstr;
-			else if (len >= LTPANEL_WIDTH - 2 * MINITEXT_PNL_X_OFFSET)
+			else if (len >= MINITEXT_PANEL_WIDTH - 2 * MINITEXT_PNL_X_OFFSET)
 				break;
 		}
 
@@ -114,7 +118,7 @@ void DrawQText()
 			pnl = endstr;
 		}
 		ty += MINITEXT_LINE_HEIGHT;
-		if (ty >= sy + TPANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET + BIG_FONT_HEIGHT) {
+		if (ty >= sy + MINITEXT_PANEL_HEIGHT - MINITEXT_PNL_Y_OFFSET + BIG_FONT_HEIGHT) {
 			break;
 		}
 	}
