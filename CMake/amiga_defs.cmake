8f8de9d243e00dbbf257f77d3393c9676897deec
Gleb Mazovetskiy <glex.spb@gmail.com>
2025-05-17 07:41:52 +0100

    amiga.cmake: Remove -fexceptions
    We no longer use exceptions anywhere in the codebase

--- CMake/amiga_defs.cmake
+++ CMake/amiga_defs.cmake
@@ -8,10 +8,6 @@ set(USE_SDL1 ON)
 set(NONET ON)
 #set(DEVILUTIONX_SYSTEM_LIBSODIUM OFF)
 
-#set(TTF_FONT_NAME \"LiberationSerif-Bold.ttf\")
-# Enable exception support as they are used in dvlnet code
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fexceptions")
-
 # Do not warn about unknown attributes, such as [[nodiscard]].
 # As this build uses an older compiler, there are lots of them.
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-attributes")
