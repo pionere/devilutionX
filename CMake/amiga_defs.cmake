356bd89d5198dc1834d81f7ef6cb91f3f0bcedd2
Gleb Mazovetskiy <glex.spb@gmail.com>
2025-05-17 07:48:54 +0100

    amiga.cmake: Remove -Wno-attributes
    Now that Amiga is on GCC 13, we no longer need this.

--- CMake/amiga_defs.cmake
+++ CMake/amiga_defs.cmake
@@ -8,9 +8,5 @@ set(USE_SDL1 ON)
 set(NONET ON)
 #set(DEVILUTIONX_SYSTEM_LIBSODIUM OFF)
 
-# Do not warn about unknown attributes, such as [[nodiscard]].
-# As this build uses an older compiler, there are lots of them.
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-attributes")
-
 # SDL video mode parameters
 set(SDL1_VIDEO_MODE_BPP 8)
