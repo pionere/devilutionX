f2fddf0d46c4c2c4a541c29ce2d48e6497597a03
pionere <pionere@freemail.hu>
2025-07-14 10:51:26 +0200

    bugfix for vanilla (rogue animation - RLH*) - fix (sync) the shield graphics with the other animations (eliminate the light rand) - improve the shadows - add missing pixels

--- tools/patcher/DiabloUI/patchdat.h
+++ tools/patcher/DiabloUI/patchdat.h
@@ -21,6 +21,16 @@ typedef struct DeltaFrameData {
 extern "C" {
 #endif
 
+extern const DeltaFrameData deltaRLHAS[3449];
+extern const DeltaFrameData deltaRLHAT[5697];
+extern const DeltaFrameData deltaRLHAW[3011];
+extern const DeltaFrameData deltaRLHST[12079];
+extern const DeltaFrameData deltaRLHBL[2243];
+extern const DeltaFrameData deltaRLHFM[1996];
+extern const DeltaFrameData deltaRLHLM[3401];
+extern const DeltaFrameData deltaRLHHT[1687];
+extern const DeltaFrameData deltaRLHQM[2993];
+extern const DeltaFrameData deltaRLHWL[3876];
 extern const DeltaFrameData deltaRLMAT[6037];
 extern const DeltaFrameData deltaRMHAT[5043];
 extern const DeltaFrameData deltaRMMAT[5280];
