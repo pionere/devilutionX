fa7cb41b3bbeffbf7c88a922839d19e300dd4337
pionere <pionere@freemail.hu>
2025-07-20 10:35:07 +0200

    ps5: update dead code in build.sh

--- Packaging/ps5/build.sh
+++ Packaging/ps5/build.sh
@@ -25,11 +25,14 @@ ${CMAKE} --build "$BUILD_DIR" -j $(getconf _NPROCESSORS_ONLN) --target package
 
 #cp -r "${SCRIPTDIR}/sce_sys" "$BUILD_DIR"/DevilutionX/
 #cp "${SCRIPTDIR}/homebrew.js" "$BUILD_DIR"/DevilutionX/
-#cp "${SCRIPTDIR}/README.md" "$BUILD_DIR"/DevilutionX/
+#cp "${SCRIPTDIR}/README.txt" "$BUILD_DIR"/DevilutionX/
 #cp "$BUILD_DIR"/devilx.mpq "$BUILD_DIR"/DevilutionX/
-#cp "$BUILD_DIR"/devilutionx "$BUILD_DIR"/DevilutionX/devilutionx.elf
+#cp "$BUILD_DIR"/../Packaging/resources/listfiles.txt "$BUILD_DIR"/DevilutionX/
+#cp "$BUILD_DIR"/../Packaging/resources/mpqfiles.txt "$BUILD_DIR"/DevilutionX/
+#cp "$BUILD_DIR"/devilutionx "$BUILD_DIR"/DevilutionX/devilutionx
+#cp "$BUILD_DIR"/devil_patcher "$BUILD_DIR"/DevilutionX/devil_patcher | true
 
 # Let github actions do this?
 #cd "$BUILD_DIR"
-#rm -f devilutionx-ps5.zip
-#zip -r devilutionx-ps5.zip DevilutionX
+#rm -f devilutionx.zip
+#zip -r devilutionx.zip DevilutionX
