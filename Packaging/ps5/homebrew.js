0db84930c9a416fc1d7fd9d664a38a99800063c1
pionere <pionere@freemail.hu>
2025-07-20 09:29:19 +0200

    ps5: use consistent white-spaces in homebrew.js

--- Packaging/ps5/homebrew.js
+++ Packaging/ps5/homebrew.js
@@ -1,26 +1,26 @@
 
 async function main() {
-    const PAYLOAD = window.workingDir + '/devilutionx.elf';
+	const PAYLOAD = window.workingDir + '/devilutionx.elf';
 
-    return {
-        mainText: "DevilutionX",
-        secondaryText: 'Diablo build for modern OSes',
-        onclick: async () => {
-	    return {
+	return {
+		mainText: "DevilutionX",
+		secondaryText: 'Diablo build for modern OSes',
+		onclick: async () => {
+		return {
 		path: PAYLOAD,
 		args: ''
-	    };
-        },
+		};
+		},
 	options: [
-	    {
+		{
 		text: "Skip Intro",
 		onclick: async () => {
-		    return {
+			return {
 			path: PAYLOAD,
 			args: '-n'
-		    };
+			};
 		}
-        }
+		}
 	]
-    };
+	};
 }
