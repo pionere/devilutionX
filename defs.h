76047763230ca9d37a3f77e06e4e800d97e90d15
pionere <pionere@freemail.hu>
2025-08-15 11:52:09 +0200

    t

--- defs.h
+++ defs.h
@@ -44,9 +44,9 @@
 #define MAXDUNX                  112
 #define MAXDUNY                  112
 /** The size of the quads in hell. */
-static_assert(DMAXX % 2 == 0, "DRLG_L4 constructs the dungeon by mirroring a quarter block -> requires to have a dungeon with even width.");
+//static_assert(DMAXX % 2 == 0, "DRLG_L4 constructs the dungeon by mirroring a quarter block -> requires to have a dungeon with even width.");
 #define L4BLOCKX (DMAXX / 2)
-static_assert(DMAXY % 2 == 0, "DRLG_L4 constructs the dungeon by mirroring a quarter block -> requires to have a dungeon with even height.");
+//static_assert(DMAXY % 2 == 0, "DRLG_L4 constructs the dungeon by mirroring a quarter block -> requires to have a dungeon with even height.");
 #define L4BLOCKY (DMAXY / 2)
 
 #ifndef NONET
