dae1d04c077c796353ef14053f547363b15c4887
pionere <pionere@freemail.hu>
2025-08-15 19:34:22 +0200

    t

--- Packaging/xbox-one/build.bat
+++ Packaging/xbox-one/build.bat
@@ -1,10 +1,10 @@
 call VsDevCmd.bat
 
-mkdir ..\..\build
-cd ..\..\build
+mkdir build
+cd build
 
-git clone https://github.com/pionere/SDL.git
-git -C SDL checkout 8b86e0ac1dd48f5c35a2b789845e248a701088b9
+rem git clone https://github.com/pionere/SDL.git
+rem git -C SDL checkout 8b86e0ac1dd48f5c35a2b789845e248a701088b9
 msbuild /p:PlatformToolset=v143;TargetPlatformVersion=10.0.22000.0;TargetPlatformMinVersion=10.0.14393.0;ConfigurationType=StaticLibrary;Configuration=Release;Platform=x64 SDL\VisualC-WinRT\SDL-UWP.vcxproj
 
 cmake -DUWP_LIB=1 -DUWP_SDL2_DIR="%CD%/SDL" -DCMAKE_BUILD_TYPE=x64-Release .. %*
