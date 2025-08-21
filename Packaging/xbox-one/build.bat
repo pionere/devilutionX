4fa5bdc9d57acb78325548f341e3e41c69d8a369
pionere <pionere@freemail.hu>
2025-08-21 08:07:42 +0200

    uwp: shortcut build when one of the steps fails

--- Packaging/xbox-one/build.bat
+++ Packaging/xbox-one/build.bat
@@ -9,18 +9,24 @@ git clone https://github.com/pionere/SDL.git
 git -C SDL checkout ede1535a4be9aa4f0d28160e4d3bb9c3d685523c
 rem - build
 msbuild /p:PlatformToolset=v143;TargetPlatformVersion=10.0.26100.0;TargetPlatformMinVersion=10.0.14393.0;ConfigurationType=StaticLibrary;Configuration=Release;Platform=x64 SDL\VisualC-WinRT\SDL-UWP.vcxproj
+if errorlevel 1 goto error
 
 rem prepare devilutionx
 rem - configure
 cmake -DUWP_LIB=1 -DUWP_SDL2_DIR="%CD%/SDL" -DCMAKE_BUILD_TYPE=x64-Release ..
+if errorlevel 1 goto error
 rem - build
 msbuild /p:Configuration=Release;Platform=x64 DevilutionX.sln
+if errorlevel 1 goto error
 
 powershell "Get-Content ..\uwp-project\Package.appxmanifest.template | %% {$_ -replace '__PROJECT_VERSION__',$(& {git describe --tags --abbrev=0})} | Out-File -FilePath ..\uwp-project\Package.appxmanifest -encoding ASCII"
 rem - build the game
 msbuild /p:Configuration=Release;Platform=x64;AppxBundle=Always;AppxBundlePlatforms=x64 ..\uwp-project\devilutionx.sln
+if errorlevel 1 goto error
 
 rem create package
 powershell "New-Item -Path uwp-project\pkg -ItemType Directory -Force; Get-Childitem -Path uwp-project\AppxPackages, uwp-project\Release, uwp-patcher\Release -Include Microsoft.VCLibs.x64.*.appx, devil*_x64.appx -File -Recurse | Copy-Item -Destination uwp-project\pkg\\"
 rem powershell "Get-Childitem -Path uwp-project\AppxPackages, uwp-project\Release -Include Microsoft.VCLibs.x64.*.appx, devil*_x64.appx -File -Recurse | Get-Unique -AsString | Compress-Archive -DestinationPath devilutionx.zip"
 powershell "Compress-Archive -Path uwp-project\pkg\* -DestinationPath devilutionx.zip"
+
+:error
