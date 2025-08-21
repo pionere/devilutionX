78986d5f8715acea45e535b359e0628fbb6ec7f6
pionere <pionere@freemail.hu>
2025-08-21 08:05:57 +0200

    uwp: adapt to local modifications

--- Packaging/xbox-one/build.bat
+++ Packaging/xbox-one/build.bat
@@ -1,18 +1,26 @@
-call VsDevCmd.bat
+rem call VsDevCmd.bat
 
-mkdir ..\..\build
-cd ..\..\build
+mkdir build
+cd build
 
-git clone https://github.com/libsdl-org/SDL.git
-git -C SDL checkout b424665e0899769b200231ba943353a5fee1b6b6
+rem prepare SDL2
+rem - checkout
+git clone https://github.com/pionere/SDL.git
+git -C SDL checkout ede1535a4be9aa4f0d28160e4d3bb9c3d685523c
+rem - build
 msbuild /p:PlatformToolset=v143;TargetPlatformVersion=10.0.26100.0;TargetPlatformMinVersion=10.0.14393.0;ConfigurationType=StaticLibrary;Configuration=Release;Platform=x64 SDL\VisualC-WinRT\SDL-UWP.vcxproj
 
+rem prepare devilutionx
+rem - configure
 cmake -DUWP_LIB=1 -DUWP_SDL2_DIR="%CD%/SDL" -DCMAKE_BUILD_TYPE=x64-Release ..
-
+rem - build
 msbuild /p:Configuration=Release;Platform=x64 DevilutionX.sln
 
 powershell "Get-Content ..\uwp-project\Package.appxmanifest.template | %% {$_ -replace '__PROJECT_VERSION__',$(& {git describe --tags --abbrev=0})} | Out-File -FilePath ..\uwp-project\Package.appxmanifest -encoding ASCII"
-
+rem - build the game
 msbuild /p:Configuration=Release;Platform=x64;AppxBundle=Always;AppxBundlePlatforms=x64 ..\uwp-project\devilutionx.sln
 
-powershell "Get-Childitem -Path uwp-project\AppxPackages, uwp-project\Release -Include Microsoft.VCLibs.x64.*.appx, devilutionX_*_x64.appx -File -Recurse | Compress-Archive -DestinationPath ..\devilutionx.zip"
+rem create package
+powershell "New-Item -Path uwp-project\pkg -ItemType Directory -Force; Get-Childitem -Path uwp-project\AppxPackages, uwp-project\Release, uwp-patcher\Release -Include Microsoft.VCLibs.x64.*.appx, devil*_x64.appx -File -Recurse | Copy-Item -Destination uwp-project\pkg\\"
+rem powershell "Get-Childitem -Path uwp-project\AppxPackages, uwp-project\Release -Include Microsoft.VCLibs.x64.*.appx, devil*_x64.appx -File -Recurse | Get-Unique -AsString | Compress-Archive -DestinationPath devilutionx.zip"
+powershell "Compress-Archive -Path uwp-project\pkg\* -DestinationPath devilutionx.zip"
