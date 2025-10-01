10f5760d53145ec37a3108b66e83431535a7736c
pionere <pionere@freemail.hu>
2025-10-01 11:24:05 +0200

    cleanup DevilutionXSDLActivity

--- android-project/app/src/main/java/org/diasurgical/devilutionx/DevilutionXSDLActivity.java
+++ android-project/app/src/main/java/org/diasurgical/devilutionx/DevilutionXSDLActivity.java
@@ -1,6 +1,5 @@
 package org.diasurgical.devilutionx;
 
-import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,7 +8,6 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewTreeObserver;
-import android.widget.Toast;
 import android.view.WindowManager;
 
 import org.libsdl.app.SDLActivity;
@@ -26,35 +24,14 @@ public class DevilutionXSDLActivity extends SDLActivity {
 	private String externalDir;
 
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
 		// windowSoftInputMode=adjustPan stopped working
 		// for fullscreen apps after Android 7.0
 		if (Build.VERSION.SDK_INT >= 25 /* Android 7.1 (N_MR1) */ )
 			trackVisibleSpace();
 
-		// Force app to overlap with the display cutout
-		if (Build.VERSION.SDK_INT >= 28 /* Android 9.0 (P) */ )
-			getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
-
 		externalDir = ExternalFilesManager.chooseExternalFilesDir(this);
-
-		super.onCreate(savedInstanceState);
-	}
-
-	/**
-	 * On app launch make sure the game data is present
-	 */
-	protected void onStart() {
-		super.onStart();
-	}
-
-	/**
-	 * When the user exits the game, use System.exit(0)
-	 * to clear memory and prevent errors on restart
-	 */
-	protected void onDestroy() {
-		super.onDestroy();
-
-		// System.exit(0);
 	}
 
 	private void trackVisibleSpace() {
