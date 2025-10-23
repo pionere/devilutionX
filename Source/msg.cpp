22fa5e7803c349d8a52997261672a89c7ff0649b
pionere <pionere@freemail.hu>
2025-10-23 09:31:05 +0200

    improve the validations of skill-use - prevent the use of WALK 'skill' in CheckPlrSkillUse - prevent the use of (WALK and) BLOCK skills in On_SKILLXY, On_SKILLMON, On_SKILLPLR

--- Source/msg.cpp
+++ Source/msg.cpp
@@ -2535,7 +2535,8 @@ static bool CheckPlrSkillUse(int pnum, const CmdSkillUse& su)
 				PlrDecMana(pnum, ma);
 			}
 		} else if (sf == SPLFROM_ABILITY) {
-			net_assert(plr._pAblSkills & SPELL_MASK(sn));
+			uint64_t mask = plr._pAblSkills & ~SPELL_MASK(SPL_WALK);
+			net_assert(mask & SPELL_MASK(sn));
 		} else {
 			net_assert((BYTE)sf < NUM_INVELEM);
 			if (!SyncUseItem(pnum, sf, sn))
@@ -2551,16 +2552,20 @@ static bool CheckPlrSkillUse(int pnum, const CmdSkillUse& su)
 static unsigned On_SKILLXY(const TCmd* pCmd, int pnum)
 {
 	const TCmdLocSkill* cmd = (const TCmdLocSkill*)pCmd;
-	int x, y;
+	int sn, x, y;
 
 	if (CheckPlrSkillUse(pnum, cmd->lsu)) {
+		sn = cmd->lsu.skill;
 		x = cmd->x;
 		y = cmd->y;
+
 		net_assert(IN_ACTIVE_AREA(x, y));
-		plr._pDestAction = spelldata[cmd->lsu.skill].sType != STYPE_NONE ? ACTION_SPELL : ((spelldata[cmd->lsu.skill].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACK : ACTION_ATTACK);
+		net_assert(/*sn != SPL_WALK &&*/ sn != SPL_BLOCK);
+
+		plr._pDestAction = spelldata[sn].sType != STYPE_NONE ? ACTION_SPELL : ((spelldata[sn].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACK : ACTION_ATTACK);
 		plr._pDestParam1 = x;
 		plr._pDestParam2 = y;
-		// plr._pDestParam3 = cmd->lsu.skill;      // spell/skill
+		// plr._pDestParam3 = sn;                  // spell/skill
 		// plr._pDestParam4 = (BYTE)cmd->lsu.from; // spllvl (set in CheckPlrSkillUse)
 	}
 
@@ -2654,15 +2659,18 @@ static unsigned On_DISARMXY(const TCmd* pCmd, int pnum)
 static unsigned On_SKILLMON(const TCmd* pCmd, int pnum)
 {
 	const TCmdMonSkill* cmd = (const TCmdMonSkill*)pCmd;
-	int mnum;
+	int sn, mnum;
 
 	if (CheckPlrSkillUse(pnum, cmd->msu)) {
+		sn = cmd->msu.skill;
 		mnum = cmd->msMnum;
 
 		net_assert(mnum < MAXMONSTERS);
-		plr._pDestAction = spelldata[cmd->msu.skill].sType != STYPE_NONE ? ACTION_SPELLMON : ((spelldata[cmd->msu.skill].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACKMON : ACTION_ATTACKMON);
+		net_assert(/*sn != SPL_WALK &&*/ sn != SPL_BLOCK);
+
+		plr._pDestAction = spelldata[sn].sType != STYPE_NONE ? ACTION_SPELLMON : ((spelldata[sn].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACKMON : ACTION_ATTACKMON);
 		plr._pDestParam1 = mnum;                // target id
-		// plr._pDestParam3 = cmd->msu.skill;      // attack spell/skill
+		// plr._pDestParam3 = sn;                  // attack spell/skill
 		// plr._pDestParam4 = (BYTE)cmd->msu.from; // attack skill-level (set in CheckPlrSkillUse)
 	}
 
@@ -2672,15 +2680,18 @@ static unsigned On_SKILLMON(const TCmd* pCmd, int pnum)
 static unsigned On_SKILLPLR(const TCmd* pCmd, int pnum)
 {
 	const TCmdPlrSkill* cmd = (const TCmdPlrSkill*)pCmd;
-	int tnum;
+	int sn, tnum;
 
 	if (CheckPlrSkillUse(pnum, cmd->psu)) {
+		sn = cmd->psu.skill;
 		tnum = cmd->psPnum;
 
 		net_assert(tnum < MAX_PLRS);
-		plr._pDestAction = spelldata[cmd->psu.skill].sType != STYPE_NONE ? ACTION_SPELLPLR : ((spelldata[cmd->psu.skill].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACKPLR : ACTION_ATTACKPLR);
+		net_assert(/*sn != SPL_WALK &&*/ sn != SPL_BLOCK);
+
+		plr._pDestAction = spelldata[sn].sType != STYPE_NONE ? ACTION_SPELLPLR : ((spelldata[sn].sUseFlags & SFLAG_RANGED) ? ACTION_RATTACKPLR : ACTION_ATTACKPLR);
 		plr._pDestParam1 = tnum;                // target id
-		// plr._pDestParam3 = cmd->psu.skill;      // attack spell/skill
+		// plr._pDestParam3 = sn;                  // attack spell/skill
 		// plr._pDestParam4 = (BYTE)cmd->psu.from; // attack skill-level (set in CheckPlrSkillUse)
 	}
 
