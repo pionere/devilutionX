cd829ddfd88d1d4e0d3694ef3b7d9587772b8c56
pionere <pionere@freemail.hu>
2026-03-23 06:41:17 +0100

    extend the logic of AddSkelMonster and rename to AddSkelMonOrObj

--- Source/themes.cpp
+++ Source/themes.cpp
@@ -517,10 +517,14 @@ done:
 	PlaceThemeMonsts(theme);
 }
 
-static void AddSkelMonster(int x, int y)
+static void AddSkelMonOrObj(BYTE rnd, int type, int x, int y)
 {
 	assert(PosOkActor(x, y));
-	AddMonster(mapSkelTypes[random_low(136, numSkelTypes)], x, y);
+	if (rnd == 0 || random_low(0, rnd) != 0) {
+		AddMonster(mapSkelTypes[random_low(136, numSkelTypes)], x, y);
+	} else {
+		AddObject(type, x, y);
+	}
 }
 
 /**
@@ -542,42 +546,21 @@ static void Theme_SkelRoom(int themeId)
 
 	monstrnd = monstrnds[currLvl._dDunType - 1]; // TODO: use dType instead?
 
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx - 1, yy - 1);
-	} else {
-		AddObject(OBJ_BANNERL, xx - 1, yy - 1);
-	}
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERL, xx - 1, yy - 1);
 
-	AddSkelMonster(xx, yy - 1);
+	AddSkelMonOrObj(0, 0, xx, yy - 1);
 
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx + 1, yy - 1);
-	} else {
-		AddObject(OBJ_BANNERR, xx + 1, yy - 1);
-	}
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx - 1, yy);
-	} else {
-		AddObject(OBJ_BANNERM, xx - 1, yy);
-	}
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx + 1, yy);
-	} else {
-		AddObject(OBJ_BANNERM, xx + 1, yy);
-	}
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx - 1, yy + 1);
-	} else {
-		AddObject(OBJ_BANNERR, xx - 1, yy + 1);
-	}
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERR, xx + 1, yy - 1);
 
-	AddSkelMonster(xx, yy + 1);
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERM, xx - 1, yy);
 
-	if (random_low(0, monstrnd) != 0) {
-		AddSkelMonster(xx + 1, yy + 1);
-	} else {
-		AddObject(OBJ_BANNERL, xx + 1, yy + 1);
-	}
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERM, xx + 1, yy);
+
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERR, xx - 1, yy + 1);
+
+	AddSkelMonOrObj(0, 0, xx, yy + 1);
+
+	AddSkelMonOrObj(monstrnd, OBJ_BANNERL, xx + 1, yy + 1);
 
 	if ((dObject[xx][yy - 3] == 0 || !objects[dObject[xx][yy - 3] - 1]._oDoorFlag)   // not a door
 	 && (nSolidTable[dPiece[xx][yy - 3]] || !nSolidTable[dPiece[xx + 1][yy - 3]])) { // or a single path to NE TODO: allow if !nSolidTable[dPiece[xx - 1][yy - 3]]?
